23553aa28aec7311a425caa7725f7cae23286bfd
J-IOProgrammer <fg.bonny@outlook.com>
2023-08-29 17:58:03 +0430

    add signIn method in person service

--- src/main/java/io/jio/todolist/services/PersonService.java
+++ src/main/java/io/jio/todolist/services/PersonService.java
@@ -1,4 +1,13 @@
 package io.jio.todolist.services;
 
+import io.jio.todolist.dto.PersonLoginDto;
+import io.jio.todolist.model.Person;
+import io.jio.todolist.repository.PersonRepository;
+
 public class PersonService {
+    PersonRepository personRepository = PersonRepository.getInstance();
+
+    public Person singIn(PersonLoginDto loginDto) {
+        return personRepository.findByUsername(loginDto);
+    }
 }
