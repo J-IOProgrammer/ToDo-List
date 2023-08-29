0f27c995db381574d74661882d55a84087e6b27f
J-IOProgrammer <fg.bonny@outlook.com>
2023-08-29 17:12:59 +0430

    add RequiredArgsConstructor and Controller anno

--- src/main/java/io/jio/todolist/controller/SingInServlet.java
+++ src/main/java/io/jio/todolist/controller/SingInServlet.java
@@ -1,8 +1,12 @@
 package io.jio.todolist.controller;
 
+import jakarta.mvc.Controller;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import lombok.RequiredArgsConstructor;
 
+@Controller
+@RequiredArgsConstructor
 @WebServlet(name = "signInServlet" , value = "/sign-in")
 public class SingInServlet extends HttpServlet {
 
