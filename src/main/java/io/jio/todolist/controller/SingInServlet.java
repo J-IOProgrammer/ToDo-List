0748867024ee894b4187818b44c0e281d3838db0
J-IOProgrammer <fg.bonny@outlook.com>
2023-08-29 16:01:01 +0430

    add new servlet SingInServlet

--- src/main/java/io/jio/todolist/controller/SingInServlet.java
+++ src/main/java/io/jio/todolist/controller/SingInServlet.java
@@ -1,9 +1,9 @@
-package io.jio.todolist.servlets;
+package io.jio.todolist.controller;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(name = "signInServlet" , value = "/sign-in")
 public class SingInServlet extends HttpServlet {
-    
+
 }
