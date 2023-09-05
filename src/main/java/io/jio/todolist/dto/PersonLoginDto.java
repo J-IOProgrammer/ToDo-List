2e524784091715bb405d144ab94b9caafe111bb8
J-IOProgrammer <fg.bonny@outlook.com>
2023-09-05 17:22:34 +0430

    add PersonLoginDto.java for login

--- src/main/java/io/jio/todolist/dto/PersonLoginDto.java
+++ src/main/java/io/jio/todolist/dto/PersonLoginDto.java
@@ -1,10 +1,12 @@
 package io.jio.todolist.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class PersonLoginDto {
     String username;
     String password;
